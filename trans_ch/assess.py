f13a02d6832f34452d51d37030e62351727b1e45
ht1602 <htian2000@icloud.com>
2021-07-22 17:35:49 +0800

    完成assess

--- trans_ch/assess.py
+++ trans_ch/assess.py
@@ -4,21 +4,44 @@ import numpy as np
 #load file
 data=pd.read_csv("../count_alp_f.csv",encoding='gbk')
 data.head()
-data=pd.DataFrame(data.drop(['v'],axis=1))
-#由于此次输入的文章中均未使用到v，故删去v的统计，方便后序信息熵的计算
+data["total"] =data.apply(lambda x:x.sum(),axis =1)
+
+#时间效率
+#每个键盘按下所需要的时间为自定义的序列，根据手放在键盘上的位置而定
+time_p=[1,3,2,2,1,1,2,2,1,1,2,2,2,2,1,2,2,2,2,3,2,2,1,3,3,2]
+time={'Alphabet': ['a', 'b', 'c', 'd','e','f','g','h','i','j','k','l','m',
+                   'n','o','p','q','r','s','t','u','v','w','x','y','z'],
+      'Time/s':time_p}
+df = pd.DataFrame(time)  # 这里默认的 index 就是 range(n)，n 是列表的长度
+df.to_csv("../time_period.csv", encoding="gbk", index=False)
+#计算输入5段100个字时间
+
+time_quanpin=data['a'].sum()*time_p[0]+data['b'].sum()*time_p[1]+data['c'].sum()*time_p[2]+data['d'].sum()*time_p[3]+data['e'].sum()*time_p[4]+data['f'].sum()*time_p[5]+data['g'].sum()*time_p[6]+data['h'].sum()*time_p[7]+data['i'].sum()*time_p[8]
+time_quanpin=time_quanpin+data['j'].sum()*time_p[9]+data['k'].sum()*time_p[10]+data['l'].sum()*time_p[11]+data['m'].sum()*time_p[12]+data['n'].sum()*time_p[13]+data['o'].sum()*time_p[14]+data['p'].sum()*time_p[15]+data['q'].sum()*time_p[16]
+time_quanpin=time_quanpin+data['r'].sum()*time_p[17]+data['s'].sum()*time_p[18]+data['t'].sum()*time_p[19]+data['u'].sum()*time_p[20]+data['v'].sum()*time_p[21]+data['w'].sum()*time_p[22]+data['x'].sum()*time_p[23]+data['y'].sum()*time_p[24]+data['z'].sum()*time_p[25]
+print("输入这些文字所需要的时间为：",time_quanpin,'s')
+#信息熵评价系统
+data=pd.DataFrame(data.drop(['v'],axis=1))#由于此次输入的文章中均未使用到v，故删去v的统计，方便后序信息熵的计算
+n = list(data.columns)
+H=0
+for i in n:
+    P=(data[i].sum())/(data["total"].sum())
+    H=H-P*np.log2(P)
+print("全拼系统的信息熵为：",H,"bit")
 
 #standardize
 # 总指标数
-n = list(data.columns)
 # 最优指标，(x-min)/(max-min)
 # 最劣指标 (max-x)/(max-min)
 # 如果指标体系存在最优指标和最劣指标，采用混合的形式
-#
+data=pd.DataFrame(data.drop(['total'],axis=1))
+n = list(data.columns)
 for i in n:
     # 获取各个指标的最大值和最小值
     Max = np.max(data[i])
     Min = np.min(data[i])
     data[i] = (data[i] - Min) / (Max - Min)
+
 
 # 建立数据比重矩阵
 for i in n:
@@ -26,20 +49,22 @@ for i in n:
     Sum = np.sum(data[i])
     # 计算某一指标占比
     data[i] = data[i] / Sum
+print("数据比重矩阵：")
 print(data)
 
 #计算信息熵e和信息效用值d
 # 统计的文章段数
 m = len(data)
 E = []
+H=[]
 # 计算信息熵值
 for i in n:
     K = 1 / np.log(m)
     e = - K * np.sum(data[i] * np.log(data[i]))
     E.append(e)
-
 # 转换为数组形式
 E = np.array(E)
+print(E)
 # 计算效用价值
 D = 1 - E
 #计算指标权重
